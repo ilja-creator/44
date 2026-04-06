f0b1502c8304b0e7896aad2c43b6a13bb2c0285b
Ilja <schnell.ilja@gmail.com>
2026-04-06 16:53:33 +0200

    commit changes

--- µ4$-z!QΩ_3^k!2/script.js
+++ µ4$-z!QΩ_3^k!2/script.js
@@ -1,11 +1,31 @@
+const file = window.location.pathname.split("/").pop().replace(".html", "");
+
 document.getElementById("Button").addEventListener("click", () => {
     const input = document.getElementById("Input").value;
     const correctPassword = "HB-44";
 
     if (input === correctPassword) {
-        document.body.innerHTML = "<h1>Notice - 1</h1><p>'text'</p>";
+        print(hint(file))
     } else {
         document.body.innerHTML = "";
         document.body.style.backgroundColor = "black";
     }
 });
+
+function hint(file) {
+    if (file === "notice-1") {
+        return "Dort, wo das Langhalstier wohnt, wirst mit dem nächsten Hinweis du belohnt."
+    }
+    if (file === "notice-2") {
+        return "Dort, wo die Liebesschlösser am nächsten hängen, soll deien Liebe zum nächsten Tipp auch beginnen."
+    }
+}
+
+function print(message) {
+    const prettyName = `Notice ${file.split("-")[1]}`
+
+    document.body.innerHTML = `
+        <h1>${prettyName}</h1>
+        <em>'${message}'</em>
+    `;
+}
